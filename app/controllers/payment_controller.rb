82144879284fabb0464e5fb96e9c5757a4b14bc6
monolok <antoinebertin@me.com>
2015-04-02 02:37:12 +0200

    design

---
+++ app/controllers/payment_controller.rb
@@ -0,0 +1,31 @@
+class PaymentController < ApplicationController
+
+	def welcome_payment
+		
+	end
+
+	def new_payment
+		
+	end
+
+	def create_payment
+		 # Amount in cents
+		@amount = 500
+
+		customer = Stripe::Customer.create(
+		:email => 'example@stripe.com',
+		:card  => params[:stripeToken]
+		)
+
+		charge = Stripe::Charge.create(
+		:customer    => customer.id,
+		:amount      => @amount,
+		:description => 'Rails Stripe customer',
+		:currency    => 'usd'
+		)
+
+		rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to charges_path
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'payment#welcome'
+  get 'payment', to: 'payment#new_payment'
+  post 'payment', to: 'payment#create_payment'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
